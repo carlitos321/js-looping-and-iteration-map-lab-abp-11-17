0dc84436e1eec96e249ca1f31bb90a91cbaa2d40
carlitos321 <ca.cabrera321@gmail.com>
2018-01-16 02:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function lowerCaseDrivers(driver) {
-  let lowercased = drivers.map(function(driverName){
+  return lowercased = drivers.map(function(driverName){
     driverName.toLowerCase();
   })
       return lowercased
