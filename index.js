44528ae0073fa2d0d6b18941be10c8cbdc0d6b2d
carlitos321 <ca.cabrera321@gmail.com>
2018-01-16 02:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file.
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function lowerCaseDrivers(driver) {
+  let lowercased = drivers.map(function(driverName){
+    drivers.toLowerCase
+  })
+  return lowercased
+}
+
+function nameToAttributes
+
+function attributesToPhrase
